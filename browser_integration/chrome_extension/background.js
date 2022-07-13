fdb0dce2353ffa16b6dd16d9b6ce46e869eb2336
Joel Baxter <joel.baxter@neogeographica.com>
2022-07-12 18:20:03 -0700

    test support for content-disposition header
    As seen in downloads from Slipgate Sightseer.
    
    Note that parsing content-disposition can be a nightmare, so I've adopted some appropriate Apache-licensed code from Mozilla. I'll just adopt Apache license for my code here too.
    
    It would be nice to put the content-disposition subroutines in a different file, but the upcoming required Manifest V3 stuff looks like it will make that a pain in the ass, so let's just cram it all into one file.

--- browser_integration/chrome_extension/background.js
+++ browser_integration/chrome_extension/background.js
@@ -1,3 +1,23 @@
+/* Copyright 2022 Joel Baxter and 2017 Mozilla Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * The first section of this file (until a comment similar to this one) is
+ * copyright Joel Baxter.
+ */
+
 const CONTEXT_MENU_ID = "OPEN_WITH_QUAKE";
 
 // The purpose of this extension is just to download a file as the
@@ -107,6 +127,7 @@ chrome.contextMenus.onClicked.addListener(
     let url = info.linkUrl;
     let didRedirect = false;
     let chunks;
+    let file;
     // Start a loop in order to support one META-refresh-style redirect.
     while (true) {
       // It would be nice to set our user agent to look like a simple
@@ -150,6 +171,17 @@ chrome.contextMenus.onClicked.addListener(
       // and go ahead to try to process it.
       // If not an HTML page that's good. Break out of this loop and proceed.
       if (!isHTML) {
+        // The filename will either be taken from the URL (using URL parsing
+        // to make sure we ignore any URL parameters) or from the
+        // Content-Disposition header if that exists.
+        const dispHeader = response.headers.get('Content-Disposition');
+        let path;
+        if (dispHeader === null) {
+            path = new URL(url).pathname;
+        } else {
+            path = getFilenameFromContentDispositionHeader(dispHeader)
+        }
+        file = path.substring(path.lastIndexOf('/') + 1);
         break;
       }
       // Hmm. This may be a page trying to redirect us with a META element,
@@ -194,10 +226,7 @@ chrome.contextMenus.onClicked.addListener(
     // Create a "blob" from the fetched chunks, with the MIME type we want.
     const blob = new Blob(chunks, {type : 'application/x-qz'});
     // Now use chrome.downloads.download to handle the blob. Add the ".qz"
-    // extension to the filename. We'll use URL parsing to make sure we
-    // ignore any URL parameters.
-    const path = new URL(url).pathname;
-    const file = path.substring(path.lastIndexOf('/') + 1);
+    // extension to the filename.
     chrome.downloads.download(
       {
         url: URL.createObjectURL(blob),
@@ -208,3 +237,225 @@ chrome.contextMenus.onClicked.addListener(
     );
   }
 );
+
+
+/*
+ * The remainder of this file is copyright Mozilla Foundation.
+ */
+
+
+// stringToBytes is from
+//   https://github.com/mozilla/pdf.js/blob/4a4c6b98515ed233b3f078422ad805beee1cd760/src/shared/util.js
+
+function stringToBytes(str) {
+  if (typeof str !== "string") {
+    unreachable("Invalid argument for stringToBytes");
+  }
+  const length = str.length;
+  const bytes = new Uint8Array(length);
+  for (let i = 0; i < length; ++i) {
+    bytes[i] = str.charCodeAt(i) & 0xff;
+  }
+  return bytes;
+}
+
+
+// The remainder of this file is from
+//   https://github.com/mozilla/pdf.js/blob/d642d345005500cdedde596ae28cd364f6ab625c/src/display/content_disposition.js
+// which in turn was adapted from
+//   https://github.com/Rob--W/open-in-browser/blob/7e2e35a38b8b4e981b11da7b2f01df0149049e92/extension/content-disposition.js
+
+
+/**
+ * Extract file name from the Content-Disposition HTTP response header.
+ *
+ * @param {string} contentDisposition
+ * @returns {string} Filename, if found in the Content-Disposition header.
+ */
+function getFilenameFromContentDispositionHeader(contentDisposition) {
+  let needsEncodingFixup = true;
+
+  // filename*=ext-value ("ext-value" from RFC 5987, referenced by RFC 6266).
+  let tmp = toParamRegExp("filename\\*", "i").exec(contentDisposition);
+  if (tmp) {
+    tmp = tmp[1];
+    let filename = rfc2616unquote(tmp);
+    filename = unescape(filename);
+    filename = rfc5987decode(filename);
+    filename = rfc2047decode(filename);
+    return fixupEncoding(filename);
+  }
+
+  // Continuations (RFC 2231 section 3, referenced by RFC 5987 section 3.1).
+  // filename*n*=part
+  // filename*n=part
+  tmp = rfc2231getparam(contentDisposition);
+  if (tmp) {
+    // RFC 2047, section
+    const filename = rfc2047decode(tmp);
+    return fixupEncoding(filename);
+  }
+
+  // filename=value (RFC 5987, section 4.1).
+  tmp = toParamRegExp("filename", "i").exec(contentDisposition);
+  if (tmp) {
+    tmp = tmp[1];
+    let filename = rfc2616unquote(tmp);
+    filename = rfc2047decode(filename);
+    return fixupEncoding(filename);
+  }
+
+  // After this line there are only function declarations. We cannot put
+  // "return" here for readability because babel would then drop the function
+  // declarations...
+  function toParamRegExp(attributePattern, flags) {
+    return new RegExp(
+      "(?:^|;)\\s*" +
+        attributePattern +
+        "\\s*=\\s*" +
+        // Captures: value = token | quoted-string
+        // (RFC 2616, section 3.6 and referenced by RFC 6266 4.1)
+        "(" +
+        '[^";\\s][^;\\s]*' +
+        "|" +
+        '"(?:[^"\\\\]|\\\\"?)+"?' +
+        ")",
+      flags
+    );
+  }
+  function textdecode(encoding, value) {
+    if (encoding) {
+      if (!/^[\x00-\xFF]+$/.test(value)) {
+        return value;
+      }
+      try {
+        const decoder = new TextDecoder(encoding, { fatal: true });
+        const buffer = stringToBytes(value);
+        value = decoder.decode(buffer);
+        needsEncodingFixup = false;
+      } catch (e) {
+        // TextDecoder constructor threw - unrecognized encoding.
+      }
+    }
+    return value;
+  }
+  function fixupEncoding(value) {
+    if (needsEncodingFixup && /[\x80-\xff]/.test(value)) {
+      // Maybe multi-byte UTF-8.
+      value = textdecode("utf-8", value);
+      if (needsEncodingFixup) {
+        // Try iso-8859-1 encoding.
+        value = textdecode("iso-8859-1", value);
+      }
+    }
+    return value;
+  }
+  function rfc2231getparam(contentDispositionStr) {
+    const matches = [];
+    let match;
+    // Iterate over all filename*n= and filename*n*= with n being an integer
+    // of at least zero. Any non-zero number must not start with '0'.
+    const iter = toParamRegExp("filename\\*((?!0\\d)\\d+)(\\*?)", "ig");
+    while ((match = iter.exec(contentDispositionStr)) !== null) {
+      let [, n, quot, part] = match; // eslint-disable-line prefer-const
+      n = parseInt(n, 10);
+      if (n in matches) {
+        // Ignore anything after the invalid second filename*0.
+        if (n === 0) {
+          break;
+        }
+        continue;
+      }
+      matches[n] = [quot, part];
+    }
+    const parts = [];
+    for (let n = 0; n < matches.length; ++n) {
+      if (!(n in matches)) {
+        // Numbers must be consecutive. Truncate when there is a hole.
+        break;
+      }
+      let [quot, part] = matches[n]; // eslint-disable-line prefer-const
+      part = rfc2616unquote(part);
+      if (quot) {
+        part = unescape(part);
+        if (n === 0) {
+          part = rfc5987decode(part);
+        }
+      }
+      parts.push(part);
+    }
+    return parts.join("");
+  }
+  function rfc2616unquote(value) {
+    if (value.startsWith('"')) {
+      const parts = value.slice(1).split('\\"');
+      // Find the first unescaped " and terminate there.
+      for (let i = 0; i < parts.length; ++i) {
+        const quotindex = parts[i].indexOf('"');
+        if (quotindex !== -1) {
+          parts[i] = parts[i].slice(0, quotindex);
+          parts.length = i + 1; // Truncates and stop the iteration.
+        }
+        parts[i] = parts[i].replace(/\\(.)/g, "$1");
+      }
+      value = parts.join('"');
+    }
+    return value;
+  }
+  function rfc5987decode(extvalue) {
+    // Decodes "ext-value" from RFC 5987.
+    const encodingend = extvalue.indexOf("'");
+    if (encodingend === -1) {
+      // Some servers send "filename*=" without encoding 'language' prefix,
+      // e.g. in https://github.com/Rob--W/open-in-browser/issues/26
+      // Let's accept the value like Firefox (57) (Chrome 62 rejects it).
+      return extvalue;
+    }
+    const encoding = extvalue.slice(0, encodingend);
+    const langvalue = extvalue.slice(encodingend + 1);
+    // Ignore language (RFC 5987 section 3.2.1, and RFC 6266 section 4.1 ).
+    const value = langvalue.replace(/^[^']*'/, "");
+    return textdecode(encoding, value);
+  }
+  function rfc2047decode(value) {
+    // RFC 2047-decode the result. Firefox tried to drop support for it, but
+    // backed out because some servers use it - https://bugzil.la/875615
+    // Firefox's condition for decoding is here: https://searchfox.org/mozilla-central/rev/4a590a5a15e35d88a3b23dd6ac3c471cf85b04a8/netwerk/mime/nsMIMEHeaderParamImpl.cpp#742-748
+
+    // We are more strict and only recognize RFC 2047-encoding if the value
+    // starts with "=?", since then it is likely that the full value is
+    // RFC 2047-encoded.
+
+    // Firefox also decodes words even where RFC 2047 section 5 states:
+    // "An 'encoded-word' MUST NOT appear within a 'quoted-string'."
+    if (!value.startsWith("=?") || /[\x00-\x19\x80-\xff]/.test(value)) {
+      return value;
+    }
+    // RFC 2047, section 2.4
+    // encoded-word = "=?" charset "?" encoding "?" encoded-text "?="
+    // charset = token (but let's restrict to characters that denote a
+    //       possibly valid encoding).
+    // encoding = q or b
+    // encoded-text = any printable ASCII character other than ? or space.
+    //        ... but Firefox permits ? and space.
+    return value.replace(
+      /=\?([\w-]*)\?([QqBb])\?((?:[^?]|\?(?!=))*)\?=/g,
+      function (matches, charset, encoding, text) {
+        if (encoding === "q" || encoding === "Q") {
+          // RFC 2047 section 4.2.
+          text = text.replace(/_/g, " ");
+          text = text.replace(/=([0-9a-fA-F]{2})/g, function (match, hex) {
+            return String.fromCharCode(parseInt(hex, 16));
+          });
+          return textdecode(charset, text);
+        } // else encoding is b or B - base64 (RFC 2047 section 4.1)
+        try {
+          text = atob(text);
+        } catch (e) {}
+        return textdecode(charset, text);
+      }
+    );
+  }
+
+  return "";
+}
